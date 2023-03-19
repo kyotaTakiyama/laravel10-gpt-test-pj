9e5024e7464e12910b6df03c007ec7b188530b6b
kyotaTakiyama <kyota.takiyama@epkotsoftware.co.jp>
2023-03-19 16:53:57 +0900

    chat_gpt関数をクラスとして切り出してリファクタリング

--- root/app/Http/Controllers/ChatGptController.php
+++ root/app/Http/Controllers/ChatGptController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
+use App\Modules\Chatgpt;
 
 class ChatGptController extends Controller
 {
@@ -35,51 +35,9 @@ class ChatGptController extends Controller
         $sentence = $request->input('sentence');
 
         // ChatGPT API処理
-        $chat_response = $this->chat_gpt("日本語で応答してください", $sentence);
+        $Chatgpt = new Chatgpt();
+        $chat_response = $Chatgpt->chat_gpt("日本語で応答してください", $sentence);
 
         return view('chat', compact('sentence', 'chat_response'));
     }
-
-    /**
-     * ChatGPT API呼び出し
-     * Laravel HTTP
-     */
-    function chat_gpt($system, $user)
-    {
-        // ChatGPT APIのエンドポイントURL
-        $url = "https://api.openai.com/v1/chat/completions";
-        // APIキー
-        $api_key = env('CHAT_GPT_KEY');
-
-        // ヘッダー
-        $headers = array(
-            "Content-Type" => "application/json",
-            "Authorization" => "Bearer $api_key"
-        );
-
-        // パラメータ
-        $data = array(
-            "model" => "gpt-3.5-turbo",
-            "messages" => [
-                [
-                    "role" => "system",
-                    "content" => $system
-                ],
-                [
-                    "role" => "user",
-                    "content" => $user
-                ]
-            ]
-        );
-
-        $response = Http::withHeaders($headers)->post($url, $data);
-
-        if ($response->json('error')) {
-            // エラー
-            return $response->json('error')['message'];
-        }
-
-        return $response->json('choices')[0]['message']['content'];
-    }
-
 }
